b718aae7f8a57219659d87004e27b430b7527eda
Nitin Salunke <nsalunke@live.com>
2021-05-12 21:39:02 +0530

    Created Azure WebJob

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -31,28 +31,36 @@ namespace VaccinatorWorker
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 string date = DateTime.Today.ToString("dd-MM-yyyy");
-                var response = await _httpClient.GetAsync($"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={_districtId}&date={date}");
-                var responseContent = await response.Content.ReadAsStringAsync();
-                var vaccineCenters = JsonConvert.DeserializeObject<Response>(responseContent);
-                _botClient.StartReceiving();
-
-                vaccineCenters.centers.ForEach(center =>
+                try
                 {
-                    if (center?.sessions?.Count > 0 && center.sessions.Any(s => s.available_capacity > 0))
-                    {
-                        SendMessageToChannel(center);
-                    }
-                });
+                    var response = await _httpClient.GetAsync($"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={_districtId}&date={date}");
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var vaccineCenters = JsonConvert.DeserializeObject<Response>(responseContent);
+                    _botClient.StartReceiving();
 
-                _logger.LogInformation("Sent message at: {time}", DateTimeOffset.Now);
-                _botClient.StopReceiving();
-                await Task.Delay(60000, stoppingToken);
+                    vaccineCenters.centers.ForEach(center =>
+                    {
+                        if (center?.sessions?.Count > 0 && center.sessions.Any(s => s.available_capacity >= 0))
+                        {
+                            SendMessageToChannel(center);
+                        }
+                    });
+
+                    _logger.LogInformation("Sent message at: {time}", DateTimeOffset.Now);
+                    _botClient.StopReceiving();
+                }
+                catch(Exception e)
+                {
+                    _logger.LogError(e);
+                }
+                
+                await Task.Delay(1000, stoppingToken);
             }
         }
 
         private void SendMessageToChannel(Center center)
         {
-            center.sessions.Where(s => s.available_capacity > 0).ToList().ForEach(async s =>
+            center.sessions.Where(s => s.available_capacity >= 0).ToList().ForEach(async s =>
             {
                 await _botClient.SendTextMessageAsync(
                                       chatId: -1001330823433,
