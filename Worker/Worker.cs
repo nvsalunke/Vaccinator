c0e0a7a3fe4eeffb89b8c7ef054304a59816a7a1
Nitin Salunke <nsalunke@live.com>
2021-05-12 22:33:48 +0530

    Added 7 days logic

--- Worker/Worker.cs
+++ Worker/Worker.cs
@@ -16,6 +16,7 @@ namespace VaccinatorWorker
         static ITelegramBotClient _botClient;
         private HttpClient _httpClient;
         private readonly int _districtId = 363;
+        private readonly long _channelChatId = -1001330823433;
 
 
         public Worker(ILogger<Worker> logger)
@@ -27,43 +28,57 @@ namespace VaccinatorWorker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await _botClient.SendTextMessageAsync(
+                                      chatId: _channelChatId,
+                                      text: $"Vaccinator started...");
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                string date = DateTime.Today.ToString("dd-MM-yyyy");
+
                 try
                 {
-                    var response = await _httpClient.GetAsync($"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={_districtId}&date={date}");
-                    var responseContent = await response.Content.ReadAsStringAsync();
-                    var vaccineCenters = JsonConvert.DeserializeObject<Response>(responseContent);
-                    _botClient.StartReceiving();
-
-                    vaccineCenters.centers.ForEach(center =>
-                    {
-                        if (center?.sessions?.Count > 0 && center.sessions.Any(s => s.available_capacity >= 0))
-                        {
-                            SendMessageToChannel(center);
-                        }
-                    });
+                    for (int i = 0; i < 7; i++) {
+                        string date = DateTime.Today.AddDays(i).ToString("dd-MM-yyyy");
+                        Processs(date);
+                    }
 
                     _logger.LogInformation("Sent message at: {time}", DateTimeOffset.Now);
-                    _botClient.StopReceiving();
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                    _logger.LogError(e);
+                    _logger.LogError(e.Message);
                 }
                 
                 await Task.Delay(1000, stoppingToken);
             }
+
+            await _botClient.SendTextMessageAsync(
+                                      chatId: _channelChatId,
+                                      text: $"Vaccinator stopped...");
+        }
+
+        private async Task Processs(string date)
+        {
+            var response = await _httpClient.GetAsync($"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={_districtId}&date={date}");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var vaccineCenters = JsonConvert.DeserializeObject<Response>(responseContent);
+
+            vaccineCenters.centers.ForEach(center =>
+            {
+                if (center?.sessions?.Count > 0 && center.sessions.Any(s => s.available_capacity > 0))
+                {
+                    SendMessageToChannel(center);
+                }
+            });
         }
 
         private void SendMessageToChannel(Center center)
         {
-            center.sessions.Where(s => s.available_capacity >= 0).ToList().ForEach(async s =>
+            center.sessions.Where(s => s.available_capacity > 0).ToList().ForEach(s =>
             {
-                await _botClient.SendTextMessageAsync(
-                                      chatId: -1001330823433,
+                _botClient.SendTextMessageAsync(
+                                      chatId: _channelChatId,
                                       text: $"Age Group: {s.min_age_limit}+\nCenter: {center.name}\nPin Code: {center.pincode}\n " +
                                       $"Available Capacity: {s.available_capacity}\nVaccine: {s.vaccine}\n Address: {center.address}"
                                     );
